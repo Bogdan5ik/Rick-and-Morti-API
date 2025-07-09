f1306e2734a4f5c08b7d6d5355e25f44e340ef39
Bogdan5ik <baggi.juk@mail.ru>
2025-07-09 14:33:11 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,21 +6,43 @@ fetch('https://rickandmortyapi.com/graphql', {
   body: JSON.stringify({
     query: `
       {
-        characters(page: 2, filter: { name: "rick" }) {
+        characters(page: 4) {
           info {
             count
           }
           results {
             name
+            image
+            status
           }
-        }
-        location(id: 1) {
-          id
-        }
-        episodesByIds(ids: [1, 2]) {
-          id
         }
       }
     `
   })
 })
+.then(res => res.json())
+.then(data => {
+  const images = document.querySelectorAll('img');
+  const nameElements = document.querySelectorAll('.names');
+  const characters = data.data.characters.results;
+  const status = document.querySelectorAll('.live');
+
+  images.forEach((img, index) => {
+    if (characters[index]) {
+      img.src = characters[index].image;
+      img.alt = characters[index].name;
+    }
+  });
+
+  nameElements.forEach((el, index) => {
+    if (characters[index]) {
+      el.innerHTML = characters[index].name;
+    }
+  });
+
+  status.forEach((le, index) => {
+    if (status[index]) {
+      le.innerHTML = characters[index].status;
+    }
+  })
+});
